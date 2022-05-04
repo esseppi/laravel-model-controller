dc1052d39448942bef4a74fe5fbe690f00d5a3c2
Your Name <you@example.com>
2022-05-04 16:15:48 +0200

    data trasmission

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
 
 class PageController extends Controller
 {
     public function index()
     {
-        return view('homePage');
+        $myMovie = Movie::all();
+        $data = [
+            'movies' => $myMovie
+        ];
+        return view('homePage', $data);
     }
 }
--- resources/views/homePage.blade.php
+++ resources/views/homePage.blade.php
@@ -1,4 +1,9 @@
 @extends('templates.base')
 @section('main')
-HOLA MAIN
+    <h1>Qesta è la pagina che lista i libri</h1>
+    <ul>
+        @foreach ($movies as $movie)
+            <li>{{ $movie->title }}</li>
+        @endforeach
+    </ul>
 @endsection
